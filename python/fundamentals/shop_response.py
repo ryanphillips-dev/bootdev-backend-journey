067ac00842ac11f40e7384e6ecfa9b85b8d4f6f6
Ryan Phillips <razza_22@hotmail.com>
2026-03-11 23:43:51 +1000

    Boot.dev: shop response exercise and notes

---
+++ python/fundamentals/shop_response.py
@@ -0,0 +1,12 @@
+print("Ah! Great choices...")
+print("Is there anything else I can help you with?")
+print("What about a new weapon?")
+print("Shopkeeper says: ok... A new weapon it is!")
+print("Shopkeeper says: ok that will be 50")
+sword_damage = 10
+start_health = 100
+end_health = start_health + sword_damage
+
+print(f"Sam's health is: {start_health}")
+print(f"Sam takes {sword_damage} damage...")
+print(f"Sam's health is: {end_health}")
